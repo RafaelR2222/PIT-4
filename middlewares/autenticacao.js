4c7d1292d0fcaa1ace59692d738497780682b975
RafaelR2222 <r3dn1t4@gmail.com>
2024-09-04 22:13:36 -0300

    feat: usuarios exibem apenas para admins, e mesmo pela url se não for admin não funciona.

--- middlewares/autenticacao.js
+++ middlewares/autenticacao.js
@@ -40,27 +40,38 @@ class Autenticacao {
     }
 
     async verificaUsuarioFuncLogado(req, res, next) {
+        res.clearCookie('isAdmin');
         if(req.headers.cookie != undefined && 
             req.headers.cookie.includes('usuarioLogado')) {
                 let id = req.cookies.usuarioLogado;
                 let usuario = new UsuarioModel();
                 usuario = await usuario.obterUsuario(id);
                 if (usuario.usuAtivo == 'S') {
+                    //cria is admin true em caso de ser admin e false caso nao
+                    if (usuario.perId === 2) {
+                        res.cookie('isAdmin', usuario.perId === 1, { httpOnly: true, maxAge: 10 * 60 * 1000 }); // Expira em 10min
+                        res.locals.usuarioLogado = usuario;
+                            
+                    } else { 
+                        res.cookie('isAdmin', usuario.perId === 1, { httpOnly: true, maxAge: 10 * 60 * 1000 }); // Expira em 10min
+                        res.locals.usuarioLogado = usuario;
+                            
+                    }
                     // Verifica se o usuário é admin ou funcionário
                     if (usuario.perId === 1 || usuario.perId === 2) {
                         // Bloqueia funcionários (perId === 2) de acessar a criação de usuários
                         if (usuario.perId === 2 && req.path.startsWith('/usuarios')) {
-                            res.redirect('/login');
+                            return res.redirect('/login');
                         }
                         res.locals.usuarioLogado = usuario;
                         next();
                     } else {
                         res.send({status: false, msg: "Você não tem permissão para acessar esta área!"});
-                        res.redirect('/login');
+                            return res.redirect('/login');
                     }
                 } else {
                     res.send({status: false, msg: "Você não é um usuário ativo!"});
-                    res.redirect('/login');
+                    return res.redirect('/login');
                 }
         } else {
             res.redirect('/login');
